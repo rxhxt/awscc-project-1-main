64f38ac9df793774d89c732875178845f516591c
Marc Brophy <aldrichbrophy@gmail.com>
2023-11-06 15:21:13 -0800

    gutted
    Signed-off-by: Marc Brophy <aldrichbrophy@gmail.com>

---
+++ src/app/about/page.js
@@ -0,0 +1,11 @@
+export default function HomePage() {
+    return (
+      <main>
+        <div id="mainHeader">
+          <img></img>
+          <h1>Extracta</h1>
+          <h2>About</h2>
+        </div>
+      </main>
+    )
+  }
