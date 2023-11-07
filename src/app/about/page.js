56862c743cf48f7512e4cbd8a2b939471586e040
Marc Brophy <aldrichbrophy@gmail.com>
2023-11-07 15:02:44 -0800

    edits
    Signed-off-by: Marc Brophy <aldrichbrophy@gmail.com>

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,11 +1,25 @@
+import styles from './about.module.css'
+import Image from 'next/image'
+import logo from '../images/logo.png'
+
+
 export default function HomePage() {
-    return (
-      <main>
-        <div id="mainHeader">
-          <img></img>
-          <h1>Extracta</h1>
-          <h2>About</h2>
-        </div>
-      </main>
-    )
-  }
+  return (
+    <main className={styles.main}>
+      <div id="mainHeader">
+      <Image src={logo} width={75} height={60}
+      style={{
+        position: 'absolute',
+        left: '2vw',
+        top: '5vh'
+      }}/>
+      <h1>Extracta</h1>
+      <h2>About</h2>
+      
+      <div id="projectDescription">
+        <h1>Project Description</h1>
+      </div>
+      </div>
+    </main>
+  )
+}
